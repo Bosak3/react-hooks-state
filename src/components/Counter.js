7d6ad59cbed4d310d107951fa24ea14e36219789
Adam <bosak3@gmail.com>
2021-11-29 22:49:42 -0600

    commit tree e9f36c4b6a41a598c764a0e6ed26af714a4dd0a2

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
 
 function Counter() {
+  //The code below uses array destructuring to shorten the syntax
   const [count, setCount] = useState(0);
+  //What the above code normally looks like
+  //
+  // const countState = useStat(0);
+  // const count= counstState[0]
+  // const setCount = countState[1];
+  //
 
   function increment() {
-    setCount(count + 1);
+    console.log(`before setState: ${count}`);
+    setCount((currentCount) => currentCount + 1);
+    setCount((currentCount) => currentCount + 1);
+    
+    
+    console.log(`after setState: ${count}`)
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
